8f6c035f5362b9187cd1feff900f382ec2fa9775
Luis Gonzalez <luismaglz@live.com>
2018-09-21 20:27:50 -0600

    this one gets me in the 1k rankings

--- podRacing.ts
+++ podRacing.ts
@@ -42,11 +42,18 @@ function isInAngleThreshold(angle: number, threshold: number): boolean {
     return (angle > threshold || angle < threshold)
 }
 
+function areWeInLine(me: Pod, enemy: Pod, checkpoint: CheckPoint) {
+    var slope1 = (enemy.positionY - enemy.positionX) / (me.positionY - me.positionX);
+    var slope2 = (checkpoint.positionY - checkpoint.positionX) / (enemy.positionY - enemy.positionX);
+    var slope3 = (checkpoint.positionY - checkpoint.positionX) / (me.positionY - me.positionX);
+    return slope1 == slope2 && slope1 == slope3;
+}
+
 var tracking = {
     frames: 0,
     boostAvailable: true,
-    track: new Array(),
-    trackMapped: false
+    trackMapped: false,
+    track: new Array()
 } as Tracking;
 
 while (true) {
@@ -56,42 +63,44 @@ while (true) {
     var myPod = new Pod(inputs[0], inputs[1]);
     var nextCheckPoint = new CheckPoint(inputs[2], inputs[3], inputs[4], inputs[5]);
     printDebug(nextCheckPoint);
-    printDebug(tracking);
-    if (!tracking.trackMapped && tracking.track.findIndex(checkpoint => checkpoint.id === nextCheckPoint.id) === -1) {
+    // printDebug(tracking);
+
+    var checkPointIndex = tracking.track.findIndex(checkpoint => checkpoint.id === nextCheckPoint.id);
+
+    if (checkPointIndex === -1) {
         tracking.track.push(nextCheckPoint);
-    } else {
-        tracking.trackMapped = true;
     }
 
+    tracking.trackMapped = tracking.track.length > 1 && checkPointIndex === 0;
+
     var inputs = readline().split(' ');
-    var opponentX = parseInt(inputs[0]);
-    var opponentY = parseInt(inputs[1]);
+    var enemyPod = new Pod(inputs[0], inputs[1]);
 
     var thrust: number | string = 100;
-    var offSet = 0;
+
 
     if (nextCheckPoint.angle > 90 || nextCheckPoint.angle < -90) {
         var angle = Math.abs(nextCheckPoint.angle);
         thrust = 30;
-    } else {
-        if (nextCheckPoint.angle !== 0) {
-            offSet = 100;
-            if (nextCheckPoint.angle < 0) {
-                offSet = -100;
-            }
-        }
+    } else if (nextCheckPoint.angle > 45 || nextCheckPoint.angle < -45) {
+        var angle = Math.abs(nextCheckPoint.angle);
+        thrust = 80;
+    }
+    else {
 
-        // if (nextCheckPoint.distance > 6000 && tracking.boostAvailable && nextCheckPoint.angle === 0 && tracking.frames > 100) {
-        //     thrust = 'BOOST';
-        //     tracking.boostAvailable = false;
-        // } else 
         if (nextCheckPoint.distance < 1500 && nextCheckPoint.angle === 0) {
             thrust = 20;
         } else if (nextCheckPoint.distance < 600) {
             thrust = 20;
         }
-    }
 
-    moveToCheckPoint(nextCheckPoint.positionX + offSet, nextCheckPoint.positionY + offSet, thrust);
+        if (tracking.boostAvailable && Math.abs(nextCheckPoint.angle) < 3 && nextCheckPoint.distance < 4000) {
+            thrust = 'BOOST';
+            tracking.boostAvailable = false;
+        }
+
+    }
+    printDebug({areWeInLine:areWeInLine(myPod, enemyPod, nextCheckPoint)});
+    moveToCheckPoint(nextCheckPoint.positionX, nextCheckPoint.positionY, thrust);
 }
 
